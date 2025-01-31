2a8b0ae0fcbe5d5a02dcc8bc82d63070bf7b773a
Bozhidar Slaveykov <bobi@microweber.com>
2025-01-31 11:53:55 +0200

    Update update-v3.sh

--- update-v3.sh
+++ update-v3.sh
@@ -13,6 +13,39 @@ WORKDIR_REL_PATH=$(pwd)/$WORKDIR
 
 cd $WORKDIR
 
+
+#### CLONE THE TEMPLATES
+SSH_KEYS_REL_PATH=$WORKDIR_REL_PATH/.SSH_KEYS
+if [ ! -d "$SSH_KEYS_REL_PATH" ]; then
+    mkdir -p $SSH_KEYS_REL_PATH
+    ssh-keygen -t rsa -b 4096 -f $SSH_KEYS_REL_PATH/id_rsa -N ""
+fi
+
+# show to user the public key to add it to the git repository
+echo "Add the following public key to the git repository:"
+cat $SSH_KEYS_REL_PATH/id_rsa.pub
+
+# Add the private key to the ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add $SSH_KEYS_REL_PATH/id_rsa
+
+# Add the private key to the ssh config
+echo "Host github.com
+  IdentityFile $SSH_KEYS_REL_PATH/id_rsa" > $SSH_KEYS_REL_PATH/config
+
+
+cd $WORKDIR_REL_PATH/microweber/Templates
+if [ ! -d "Big2" ]; then
+    git clone git@github.com:microweber-templates/big2.git Big2
+fi
+
+git pull
+
+rsync -a --quiet $WORKDIR_REL_PATH/microweber/Templates/ $PROJECT_DIR/Templates/
+
+
+## CLONE THE MICROWEBER REPOSITORY
+
 # Check if the folder exists
 if [ ! -d "microweber" ]; then
     # Clone the repository
@@ -43,33 +76,6 @@ rsync -a --quiet $WORKDIR_REL_PATH/microweber/app/ $PROJECT_DIR/app/
 rsync -a --quiet $WORKDIR_REL_PATH/microweber/bootstrap/ $PROJECT_DIR/bootstrap/
 rsync -a --quiet $WORKDIR_REL_PATH/microweber/resources/ $PROJECT_DIR/resources/
 rsync -a --quiet $WORKDIR_REL_PATH/microweber/public/ $PROJECT_DIR/public/
-#
-#
-SSH_KEYS_REL_PATH=$WORKDIR_REL_PATH/.SSH_KEYS
-if [ ! -d "$SSH_KEYS_REL_PATH" ]; then
-    mkdir -p $SSH_KEYS_REL_PATH
-    ssh-keygen -t rsa -b 4096 -f $SSH_KEYS_REL_PATH/id_rsa -N ""
-fi
-
-# show to user the public key to add it to the git repository
-echo "Add the following public key to the git repository:"
-cat $SSH_KEYS_REL_PATH/id_rsa.pub
-
-# Add the private key to the ssh-agent
-eval "$(ssh-agent -s)"
-ssh-add $SSH_KEYS_REL_PATH/id_rsa
-
-# Add the private key to the ssh config
-echo "Host github.com
-  IdentityFile $SSH_KEYS_REL_PATH/id_rsa" > $SSH_KEYS_REL_PATH/config
 
 
-cd $WORKDIR_REL_PATH/microweber/Templates
-if [ ! -d "Big2" ]; then
-    git clone git@github.com:microweber-templates/big2.git Big2
-fi
-
-git pull
-
-rsync -a --quiet $WORKDIR_REL_PATH/microweber/Templates/ $PROJECT_DIR/Templates/
-
+echo "The update is completed."
